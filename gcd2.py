86c2fc01ef162264a093e472cf43a6f4ddff59cd
saikiran deshmukh <saikirandeshmukh28@gmail.com>
2026-03-03 06:53:11 -0800

    not list used

---
+++ gcd2.py
@@ -0,0 +1,16 @@
+#no list
+def gcd(m,n):
+    for i in range(1,min(m,n)+1):
+        if ((m%i)==0) and ((n%i)==0):
+            k=i
+    return k
+
+print(gcd(12,16))
+
+"""
+tracing:
+gcd(12,16)
+i->> (1,12+1)-->(1,13)
+k=4
+reutrns 4
+"""
